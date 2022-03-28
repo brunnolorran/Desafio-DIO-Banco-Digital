94097d4ad344bdfccad6dd6205b34408b5472d9d
brunnolorran <brunnolorran.silva@gmail.com>
2022-03-28 13:41:17 -0300

    Create heritage

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,30 +1,3 @@
-public class ContaCorrente {
+public class ContaCorrente extends Conta {
 
-    private int agencia;
-    private int numero;
-    private double saldo;
-
-    public void sacar() {
-        
-    }
-
-    public void depositar() {
-        
-    }
-
-    public void reanferir() {
-        
-    }
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
 }
